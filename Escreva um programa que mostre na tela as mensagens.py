384ad15b024b4b3fc16538c51bcb0b0488a59a9b
camilabsp <120345334+camilabsp@users.noreply.github.com>
2023-02-17 09:49:59 -0300

    lista 1 - questão 1

---
+++ Escreva um programa que mostre na tela as mensagens.py
@@ -0,0 +1,15 @@
+''' Escreva um programa que mostre na tela as mensagens:
+
+ Alô, mundo!
+ALO MUNDO
+alô, mundo.
+ALo munDO
+Alô, Mundo!
+
+'''
+
+print('Alô, mundo!')
+print('ALO MUNDO')
+print('alô, mundo.')
+print('ALo munDO')
+print('Alô, Mundo!')
