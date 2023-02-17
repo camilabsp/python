54e8f66bd3c9c50ac69c963be860cccd53e744e7
camilabsp <120345334+camilabsp@users.noreply.github.com>
2023-02-17 10:12:46 -0300

    q1-lista1
    Escreva um programa que mostre na tela as mensagens:

--- nivel_1/Escreva um programa que leia separadamente seu nome e o seu sobrenome e mostre na tela o nome completo..py
+++
@@ -1,6 +0,0 @@
-' Escreva um programa que leia separadamente seu nome e o seu sobrenome e mostre na tela o nome completo.'
-
-nome = str(input('Digite seu nome: '))
-sobrenome = str(input('Digite seu sobrenome: '))
-
-print(nome + ' ' + sobrenome)
