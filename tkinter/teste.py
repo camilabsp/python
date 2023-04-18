b4e05cf563e5e31dc0e52d901c214f466c06fcda
camilabsp <120345334+camilabsp@users.noreply.github.com>
2023-04-18 15:50:05 -0300

    Update teste.py

--- tkinter/teste.py
+++ tkinter/teste.py
@@ -9,7 +9,7 @@ class app():
         self.root = root
         self.tela()
         self.frames()
-        self.botao()
+        self.widgets_frame1()
         root.mainloop()
     def tela(self):
         self.root.title('Otimizador de Investimentos')
@@ -18,16 +18,26 @@ class app():
     
     def frames(self):
         self.frame1 = customtkinter.CTkFrame(self.root)
-        self.frame1.place(relx = 0,rely = 0,relwidth = 1,relheight = 0.30)
+        self.frame1.place(relx = 0,rely = 0,relwidth = 1,relheight = 0.20)
         
+
     
-    def botao(self):
-        self.bt_add = customtkinter.CTkButton (self.frame1,text = 'Adicionar')
-        self.bt_add.place(relx = 0.1, rely = 0.9)
-        self.bt_apagar = customtkinter.CTkButton (self.frame1,text = 'Apagar')
-        self.bt_apagar.place(relx = 0.5, rely = 0.9)
+    def widgets_frame1(self):
+        self.bt_add = customtkinter.CTkButton (self.frame1,text = 'Adicionar', width = 70)
+        self.bt_add.place(relx = 0.8, rely = 0.2)
+        self.bt_apagar = customtkinter.CTkButton (self.frame1,text = 'Apagar', width = 70)
+        self.bt_apagar.place(relx = 0.8, rely = 0.6)
 
+        
+        self.compra = customtkinter.CTkRadioButton(self.frame1,text = 'Comprar')
+        self.compra.place(relx = 0.65, rely = 0.2)
+        self.venda = customtkinter.CTkRadioButton(self.frame1,text = 'Vender')
+        self.venda.place(relx = 0.65, rely = 0.6)
+        
 
+    def widgets_frame2(self):
+        pass
+        
 
 
 app()
